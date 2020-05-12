151eac94006fcbc280d287a25a375b7d641cf6f1
Luke <luke@coldbrewstudios.co.uk>
2020-05-12 17:59:41 +0100

    feature: simplified render

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Main from './Main';
 
-export class App {
-	private _appName: string = 'Luke in the Cloud';
-
-	constructor() {
-		console.log('Nice');
-		this.render();
-	}
-
-	private render(): void {
-		ReactDOM.render(React.createElement(Main, {app: this}), document.getElementById('root'));
-	}
-
-	public get appName(): string {
-		return this._appName;
-	}
-}
-
-new App();
+ReactDOM.render(React.createElement(Main), document.getElementById('root'));
